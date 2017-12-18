db6c21c2dcf3a5177d46dde753369b3fd15f293d
pxgamer <owzie123@gmail.com>
2017-12-18 13:24:32 +0000

    Added Hosts class

---
+++ src/Hosts.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace YeTii\SiteManager;
+
+class Hosts
+{
+    const ERROR_UNABLE_TO_ADD = 'Unable to add the specified vhost.';
+    const ERROR_UNABLE_TO_REMOVE = 'Unable to remove the specified vhost.';
+
+    const DEFAULT_MAC_PATH = '/etc/hosts';
+
+    public static function get(string $n1 = null)
+    {
+        return !is_null($n1) ?
+            <<<TXT
+127.0.0.1       {$n1}.local
+
+TXT
+            : '';
+    }
+}
